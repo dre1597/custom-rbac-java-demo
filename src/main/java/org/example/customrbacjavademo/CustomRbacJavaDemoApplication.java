0fc7a2f5408797220bc3767fd9c005a1c4e521c6
André Pereira <andrepereira97@yahoo.com>
2025-03-01 10:55:00 -0300

    :tada: start project

---
+++ src/main/java/org/example/customrbacjavademo/CustomRbacJavaDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.customrbacjavademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomRbacJavaDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CustomRbacJavaDemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/customrbacjavademo/CustomRbacJavaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.customrbacjavademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomRbacJavaDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
