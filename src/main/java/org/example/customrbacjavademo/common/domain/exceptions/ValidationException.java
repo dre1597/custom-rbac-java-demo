ee9a254db3f1e8bf8a61818a76fd85aab2788ba4
André Pereira <andrepereira97@yahoo.com>
2025-04-15 20:39:00 -0300

    :coffin: remove unused method

--- src/main/java/org/example/customrbacjavademo/common/domain/exceptions/ValidationException.java
+++ src/main/java/org/example/customrbacjavademo/common/domain/exceptions/ValidationException.java
@@ -3,19 +3,11 @@ package org.example.customrbacjavademo.common.domain.exceptions;
 import java.util.List;
 
 public class ValidationException extends RuntimeException {
-  private final List<String> errors;
-
   public ValidationException(final List<String> errors) {
     super(String.join(", ", errors));
-    this.errors = errors;
   }
 
   public ValidationException(final String message) {
     super(message);
-    this.errors = List.of(message);
-  }
-
-  public List<String> getErrors() {
-    return errors;
   }
 }
