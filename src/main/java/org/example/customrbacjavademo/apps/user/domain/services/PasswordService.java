94a56872ee4670f1c4870c597c24685d28df02cf
André Pereira <andrepereira97@yahoo.com>
2025-04-20 16:43:44 -0300

    :sparkles: change dependency for bcrypt

--- src/main/java/org/example/customrbacjavademo/apps/user/domain/services/PasswordService.java
+++ src/main/java/org/example/customrbacjavademo/apps/user/domain/services/PasswordService.java
@@ -1,19 +1,16 @@
 package org.example.customrbacjavademo.apps.user.domain.services;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.mindrot.jbcrypt.BCrypt;
 
 public final class PasswordService {
-  private static final PasswordEncoder encoder = new BCryptPasswordEncoder();
-
   private PasswordService() {
   }
 
   public static String encryptPassword(final String rawPassword) {
-    return encoder.encode(rawPassword);
+    return BCrypt.hashpw(rawPassword, BCrypt.gensalt());
   }
 
-  public static boolean matches(final String rawPassword, final String encodedPassword) {
-    return encoder.matches(rawPassword, encodedPassword);
+  public static boolean matches(final String rawPassword, final String hashedPassword) {
+    return BCrypt.checkpw(rawPassword, hashedPassword);
   }
 }
