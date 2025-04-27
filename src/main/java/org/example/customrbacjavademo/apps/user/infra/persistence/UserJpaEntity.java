99be6487501edc1d01af3efdcf667b5a6698fc97
André Pereira <andrepereira97@yahoo.com>
2025-04-27 00:53:00 -0300

    :fire: remove unnecessary code

--- src/main/java/org/example/customrbacjavademo/apps/user/infra/persistence/UserJpaEntity.java
+++ src/main/java/org/example/customrbacjavademo/apps/user/infra/persistence/UserJpaEntity.java
@@ -67,27 +67,7 @@ public class UserJpaEntity implements UserDetails {
   public String getUsername() {
     return this.name;
   }
-
-  @Override
-  public boolean isAccountNonExpired() {
-    return true;
-  }
-
-  @Override
-  public boolean isAccountNonLocked() {
-    return true;
-  }
-
-  @Override
-  public boolean isCredentialsNonExpired() {
-    return true;
-  }
-
-  @Override
-  public boolean isEnabled() {
-    return true;
-  }
-
+  
   public UUID getId() {
     return id;
   }
