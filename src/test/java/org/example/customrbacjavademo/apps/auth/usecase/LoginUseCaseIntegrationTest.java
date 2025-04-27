f41f45ad4394948b68d18829bb296905b1ba1bd2
André Pereira <andrepereira97@yahoo.com>
2025-04-27 00:51:08 -0300

    :white_check_mark: add login use case tests

---
+++ src/test/java/org/example/customrbacjavademo/apps/auth/usecase/LoginUseCaseIntegrationTest.java
@@ -0,0 +1,121 @@
+package org.example.customrbacjavademo.apps.auth.usecase;
+
+import org.example.customrbacjavademo.IntegrationTest;
+import org.example.customrbacjavademo.apps.auth.domain.dto.LoginDto;
+import org.example.customrbacjavademo.apps.user.domain.mocks.PermissionTestMocks;
+import org.example.customrbacjavademo.apps.user.domain.mocks.RoleTestMocks;
+import org.example.customrbacjavademo.apps.user.domain.mocks.UserTestMocks;
+import org.example.customrbacjavademo.apps.user.infra.persistence.PermissionJpaRepository;
+import org.example.customrbacjavademo.apps.user.infra.persistence.RoleJpaRepository;
+import org.example.customrbacjavademo.apps.user.infra.persistence.UserJpaRepository;
+import org.example.customrbacjavademo.apps.user.usecase.permission.mappers.PermissionMapper;
+import org.example.customrbacjavademo.apps.user.usecase.role.mappers.RoleMapper;
+import org.example.customrbacjavademo.apps.user.usecase.user.mappers.UserMapper;
+import org.example.customrbacjavademo.common.domain.exceptions.ValidationException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.AuthenticationException;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@IntegrationTest
+class LoginUseCaseIntegrationTest {
+  @Autowired
+  private LoginUseCase useCase;
+
+  @Autowired
+  private UserJpaRepository repository;
+
+  @Autowired
+  private RoleJpaRepository roleRepository;
+
+  @Autowired
+  private PermissionJpaRepository permissionRepository;
+
+  @Test
+  void shouldLogin() {
+    final var permission = permissionRepository.save(
+        PermissionMapper.entityToJpa(PermissionTestMocks.createActiveTestPermission())
+    );
+    final var role = roleRepository.save(
+        RoleMapper.entityToJpa(RoleTestMocks.createActiveTestRole(List.of(permission.getId())))
+    );
+    final var user = repository.save(UserMapper.entityToJpa(UserTestMocks.createActiveTestUser(role.getId())));
+    final var dto = LoginDto.of(user.getName(), "any_password");
+
+    final var response = useCase.execute(dto);
+
+    assertNotNull(response.token());
+    assertEquals(user.getId().toString(), response.user().id());
+    assertEquals(user.getName(), response.user().name());
+    assertEquals(user.getRole().getId().toString(), response.user().roleId());
+    assertEquals(user.getRole().getName(), response.user().roleName());
+  }
+
+  @Test
+  void shouldThrowWhenAuthenticationFails() {
+    final var permission = permissionRepository.save(
+        PermissionMapper.entityToJpa(PermissionTestMocks.createActiveTestPermission())
+    );
+    final var role = roleRepository.save(
+        RoleMapper.entityToJpa(RoleTestMocks.createActiveTestRole(List.of(permission.getId())))
+    );
+    final var user = repository.save(UserMapper.entityToJpa(UserTestMocks.createActiveTestUser(role.getId())));
+
+    final var dto = LoginDto.of(user.getName(), "invalid_password");
+
+    final var exception = assertThrows(
+        AuthenticationException.class,
+        () -> useCase.execute(dto)
+    );
+
+    assertEquals("Bad credentials", exception.getMessage());
+  }
+
+  @Test
+  void shouldThrowWhenUserNotFoundAfterAuthentication() {
+    final var loginDto = new LoginDto("authenticated_but_not_found", "password");
+
+    final var exception = assertThrows(AuthenticationException.class,
+        () -> useCase.execute(loginDto));
+
+    assertEquals("Bad credentials", exception.getMessage());
+  }
+
+  @ParameterizedTest
+  @NullAndEmptySource
+  void shouldThrowWhenUsernameIsEmpty(final String username) {
+    final var loginDto = new LoginDto(username, "password");
+
+    final var exception = assertThrows(ValidationException.class,
+        () -> useCase.execute(loginDto));
+
+    assertEquals("name is required", exception.getMessage());
+  }
+
+  @ParameterizedTest
+  @NullAndEmptySource
+  void shouldThrowWhenPasswordIsEmpty(final String password) {
+    final var loginDto = new LoginDto("username", password);
+
+    final var exception = assertThrows(ValidationException.class,
+        () -> useCase.execute(loginDto));
+
+    assertEquals("password is required", exception.getMessage());
+  }
+
+  @ParameterizedTest
+  @NullAndEmptySource
+  void shouldThrowWhenUsernameAndPasswordAreEmpty(final String input) {
+    final var loginDto = new LoginDto(input, input);
+
+    final var exception = assertThrows(ValidationException.class,
+        () -> useCase.execute(loginDto));
+
+    assertEquals("name is required, password is required", exception.getMessage());
+  }
+}
---
+++ src/test/java/org/example/customrbacjavademo/apps/auth/usecase/LoginUseCaseTest.java
@@ -0,0 +1,152 @@
+package org.example.customrbacjavademo.apps.auth.usecase;
+
+import org.example.customrbacjavademo.apps.auth.domain.dto.LoginDto;
+import org.example.customrbacjavademo.apps.auth.domain.services.JwtService;
+import org.example.customrbacjavademo.apps.user.domain.mocks.UserTestMocks;
+import org.example.customrbacjavademo.apps.user.infra.persistence.UserJpaRepository;
+import org.example.customrbacjavademo.apps.user.usecase.user.mappers.UserMapper;
+import org.example.customrbacjavademo.common.domain.exceptions.NotFoundException;
+import org.example.customrbacjavademo.common.domain.exceptions.ValidationException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class LoginUseCaseTest {
+  @Mock
+  private UserJpaRepository userJpaRepository;
+
+  @Mock
+  private AuthenticationManager authenticationManager;
+
+  @Mock
+  private JwtService jwtService;
+
+  @InjectMocks
+  private LoginUseCase useCase;
+
+  @Test
+  void shouldLogin() {
+    final var password = "any_password";
+    final var user = UserTestMocks.createActiveTestUser("any_name", password);
+
+    when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+        .thenReturn(new UsernamePasswordAuthenticationToken(user.getName(), password));
+    when(userJpaRepository.findWithRoleByName(user.getName()))
+        .thenReturn(Optional.of(UserMapper.entityToJpa(user)));
+
+    final var result = useCase.execute(new LoginDto(user.getName(), password));
+
+    verify(authenticationManager).authenticate(any(UsernamePasswordAuthenticationToken.class));
+    verify(userJpaRepository).findWithRoleByName(user.getName());
+
+    System.out.println(result);
+
+    assertEquals(user.getId().toString(), result.user().id());
+    assertEquals(user.getName(), result.user().name());
+  }
+
+  @Test
+  void shouldThrowWhenAuthenticationFails() {
+    final var loginDto = new LoginDto("invalid_user", "wrong_password");
+
+    when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+        .thenThrow(new BadCredentialsException("Bad credentials"));
+
+    assertThrows(BadCredentialsException.class, () -> useCase.execute(loginDto));
+
+    verify(authenticationManager).authenticate(
+        new UsernamePasswordAuthenticationToken("invalid_user", "wrong_password")
+    );
+    verify(userJpaRepository, never()).findWithRoleByName(anyString());
+  }
+
+  @Test
+  void shouldThrowWhenUserNotFoundAfterAuthentication() {
+    final var loginDto = new LoginDto("authenticated_but_not_found", "password");
+
+    when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+        .thenReturn(null);
+    when(userJpaRepository.findWithRoleByName("authenticated_but_not_found"))
+        .thenReturn(Optional.empty());
+
+    final var exception = assertThrows(NotFoundException.class,
+        () -> useCase.execute(loginDto));
+
+    assertEquals("User not found", exception.getMessage());
+
+    verify(authenticationManager).authenticate(
+        new UsernamePasswordAuthenticationToken("authenticated_but_not_found", "password")
+    );
+    verify(userJpaRepository).findWithRoleByName("authenticated_but_not_found");
+  }
+
+  @ParameterizedTest
+  @NullAndEmptySource
+  void shouldThrowWhenUsernameIsEmpty(final String username) {
+    final var loginDto = new LoginDto(username, "password");
+
+    final var exception = assertThrows(ValidationException.class,
+        () -> useCase.execute(loginDto));
+
+    assertEquals("name is required", exception.getMessage());
+
+    verify(authenticationManager, never()).authenticate(any());
+    verify(userJpaRepository, never()).findWithRoleByName(any());
+  }
+
+  @ParameterizedTest
+  @NullAndEmptySource
+  void shouldThrowWhenPasswordIsEmpty(final String password) {
+    final var loginDto = new LoginDto("username", password);
+
+    final var exception = assertThrows(ValidationException.class,
+        () -> useCase.execute(loginDto));
+
+    assertEquals("password is required", exception.getMessage());
+
+    verify(authenticationManager, never()).authenticate(any());
+    verify(userJpaRepository, never()).findWithRoleByName(any());
+  }
+
+  @Test
+  void shouldThrowWhenUsernameAndPasswordAreNull() {
+    final var loginDto = new LoginDto(null, null);
+
+    final var exception = assertThrows(ValidationException.class,
+        () -> useCase.execute(loginDto));
+
+    assertEquals("name is required, password is required", exception.getMessage());
+
+    verify(authenticationManager, never()).authenticate(any());
+    verify(userJpaRepository, never()).findWithRoleByName(any());
+  }
+
+  @ParameterizedTest
+  @NullAndEmptySource
+  void shouldThrowWhenUsernameAndPasswordAreEmpty(final String input) {
+    final var loginDto = new LoginDto(input, input);
+
+    final var exception = assertThrows(ValidationException.class,
+        () -> useCase.execute(loginDto));
+
+    assertEquals("name is required, password is required", exception.getMessage());
+
+    verify(authenticationManager, never()).authenticate(any());
+    verify(userJpaRepository, never()).findWithRoleByName(any());
+  }
+}
