b1354436d4aa633b580aebcca71cb70d54055d06
André Pereira <andrepereira97@yahoo.com>
2025-04-27 00:59:48 -0300

    :white_check_mark: add auth controller test

---
+++ src/test/java/org/example/customrbacjavademo/apps/auth/infra/api/controllers/AuthControllerTest.java
@@ -0,0 +1,36 @@
+package org.example.customrbacjavademo.apps.auth.infra.api.controllers;
+
+import org.example.customrbacjavademo.apps.auth.domain.dto.LoginDto;
+import org.example.customrbacjavademo.apps.auth.infra.api.dto.requests.LoginRequest;
+import org.example.customrbacjavademo.apps.auth.infra.api.dto.responses.LoginResponse;
+import org.example.customrbacjavademo.apps.auth.infra.api.dto.responses.UserLoginResponse;
+import org.example.customrbacjavademo.apps.auth.usecase.LoginUseCase;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AuthControllerTest {
+  @Mock
+  private LoginUseCase loginUseCase;
+
+  @InjectMocks
+  private AuthController controller;
+
+  @Test
+  void shouldLogin() {
+    final var input = new LoginRequest("any_name", "any_password");
+    final var userLoginResponse = new UserLoginResponse("any_id", "any_name", "any_role_id", "any_role_name");
+    final var expectedResponse = new LoginResponse(userLoginResponse, "any_token");
+
+    when(loginUseCase.execute(LoginDto.from(input))).thenReturn(expectedResponse);
+    controller.login(input);
+
+    verify(loginUseCase).execute(LoginDto.from(input));
+  }
+}
